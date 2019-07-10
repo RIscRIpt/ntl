7491499aa0b57be009744edcf2495cb1456ab64f
RIscRIpt <richard@riscript.com>
2019-07-10 10:41:23 +0300

    implement allocator, remove iterator, add system type_traits utility

--- ntl/ntl.cpp
+++ ntl/ntl.cpp
@@ -1,11 +1,14 @@
-#include <ntddk.h>
-
 #include "ntl"
+#include "system"
 
 void* __cdecl operator new(size_t count) {
-    return ExAllocatePoolWithTag(NonPagedPoolNx, count, NTL_TAG);
+    void *result = ExAllocatePoolWithTag(NonPagedPoolNx, count, NTL_TAG);
+    if (!result) {
+        ExRaiseStatus(STATUS_FATAL_MEMORY_EXHAUSTION);
+    }
+    return result;
 }
 
-void __cdecl operator delete(void* object) {
+void __cdecl operator delete(void *object) {
     ExFreePoolWithTag(object, NTL_TAG);
 }
