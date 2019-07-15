f189b989f57bd7c0bfe46aa4183d7489e615dbc9
RIscRIpt <richard@riscript.com>
2019-07-15 19:48:21 +0300

    add DebugInUserMode configuration

--- ntl/ntl.cpp
+++ ntl/ntl.cpp
@@ -1,6 +1,8 @@
 #include "ntl"
 #include "system"
 
+#if !DEBUG_IN_USER_MODE
+
 void* __cdecl operator new(size_t count) {
     void *result = ExAllocatePoolWithTag(NonPagedPoolNx, count, NTL_TAG);
     if (!result) {
@@ -12,3 +14,19 @@ void* __cdecl operator new(size_t count) {
 void __cdecl operator delete(void *object) {
     ExFreePoolWithTag(object, NTL_TAG);
 }
+
+#else
+
+void* __cdecl operator new(size_t count) {
+    void *result = malloc(count);
+    if (!result) {
+        SetLastError(ERROR_OUTOFMEMORY);
+    }
+    return result;
+}
+
+void __cdecl operator delete(void *object) {
+    free(object);
+}
+
+#endif
